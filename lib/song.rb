13f3d02753304124e7033ef36b9e5eacc7419cf7
Manny Pissanos <mpissano@gmail.com>
2018-10-01 16:48:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   end
   
   def genre_count
-    g = Hash[ @@genres.collect { |value| [ value, f(v)] }
+    g = Hash[ @@genres.collect { |value| [ value, @@genres(value)] }
   end
   
   def artis_count
